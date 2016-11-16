5b0e573204d979f0818a44de85cf9492df3a8b54
Joshua Harms <joshua@nozzlegear.com>
2016-11-16 16:38:51 -0600

    Remove old views

--- old_views/crumb.tsx
+++
@@ -1,8 +0,0 @@
-/// <reference path="./../typings/typings.d.ts" />
-
-import * as React from "react";
-
-export function Crumb(props: {value: string})
-{
-    return <input type="hidden" name="crumb" value={props.value} />
-}
